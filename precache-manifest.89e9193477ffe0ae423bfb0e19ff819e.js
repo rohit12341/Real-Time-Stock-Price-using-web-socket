1e4f13f37e098f1e6b6f93d0a6b5221a53d711d4
rohitmeena12 <rohit.meena@immortal-technologies.com>
2020-07-23 12:51:05 +0530

    Updates

---
+++ precache-manifest.89e9193477ffe0ae423bfb0e19ff819e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4a08b2d36a613075924cd2a93bfd50",
+    "url": "/rohit12341/Real-Time-Stock-Price-using-web-socket/index.html"
+  },
+  {
+    "revision": "c4069ca6ee9ebe3de522",
+    "url": "/rohit12341/Real-Time-Stock-Price-using-web-socket/static/css/main.a7051032.chunk.css"
+  },
+  {
+    "revision": "e6858f995c1b4793439e",
+    "url": "/rohit12341/Real-Time-Stock-Price-using-web-socket/static/js/2.d69ee06d.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/rohit12341/Real-Time-Stock-Price-using-web-socket/static/js/2.d69ee06d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4069ca6ee9ebe3de522",
+    "url": "/rohit12341/Real-Time-Stock-Price-using-web-socket/static/js/main.dd99fb87.chunk.js"
+  },
+  {
+    "revision": "7332bf8c98b3224d05a5",
+    "url": "/rohit12341/Real-Time-Stock-Price-using-web-socket/static/js/runtime-main.55a11af2.js"
+  }
+]);
